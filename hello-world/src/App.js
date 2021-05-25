babcf7a93c3599dd97381f22f0d8d478468ad2b9
Mindy Medeiros <mmedei6@wgu.edu>
2021-05-25 06:44:06 -0400

    Change link to GitHub repo

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mindy-m/animated-garbanzo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out the repo!
         </a>
       </header>
     </div>
